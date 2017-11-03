e61e787da5533ee5019efb00f8271a944ffc4c30
Aqeel Akber <aqeel.akber@gmail.com>
2017-11-03 23:42:04 +1100

    More structure

--- airom/findpose.py
+++ airom/findpose.py
@@ -1,7 +1,6 @@
 import subprocess
 
-def ProcessVideo(infile, outdir, options=""):
+def VideoToJSON(infile, outdir, options=""):
     subprocess.call(["../openpose/build/examples/openpose/openpose.bin",
-                     "--no-display",
+                     "--no-display", "--write_images"+outdir,
                      "--video "+infile, "--output "+outdir])
-    
